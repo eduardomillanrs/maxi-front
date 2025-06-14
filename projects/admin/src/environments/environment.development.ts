a69a5ec46af0fc438a2ed179b481e94580f661c0
Eduardo Millan <eduardomillanrs@outlook.com>
2025-06-13 23:31:27 -0600

    Add environments

---
+++ projects/admin/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import { Environment } from '@admin/app/shared/types/environment';
+
+export const environment: Environment = {
+  production: false,
+  backendUrl: 'http://localhost:8000/api',
+};
---
+++ projects/admin/src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from '@admin/app/shared/types/environment';
+
+export const environment: Environment = {
+  production: true,
+  backendUrl: '',
+};
