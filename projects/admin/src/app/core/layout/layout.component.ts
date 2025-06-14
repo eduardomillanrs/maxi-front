f220bad34175fc4cc4b80ce1c3f53b2dbd1151a6
Eduardo Millan <eduardomillanrs@outlook.com>
2025-06-13 23:35:24 -0600

    Update layouts children components paths

--- projects/admin/src/app/core/layout/layout.component.ts
+++ projects/admin/src/app/core/layout/layout.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent, SidebarComponent } from '@admin/app/core/layout/components';
 
 @Component({
   selector: 'app-layout',
