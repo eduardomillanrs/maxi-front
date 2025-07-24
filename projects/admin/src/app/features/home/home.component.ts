175ea64ef387d9fb2eb88b36cb9a43840ae88c17
Eduardo Millan <eduardomillanrs@outlook.com>
2025-07-23 18:26:17 -0600

    Update home playground

--- projects/admin/src/app/features/home/home.component.ts
+++ projects/admin/src/app/features/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+import { PageComponent } from '@admin/app/shared/components';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule, DatePickerModule, FloatLabelModule, PageComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
