276bff85a06100f045e6e5410e5873854b42d563
Eduardo Millan <eduardomillanrs@outlook.com>
2025-07-31 18:23:08 -0600

    Set transition effect when dom is loaded

--- projects/admin/src/app/core/layout/layout.component.ts
+++ projects/admin/src/app/core/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent, SidebarComponent } from '@admin/app/core/layout/components';
@@ -9,4 +9,11 @@ import { HeaderComponent, SidebarComponent } from '@admin/app/core/layout/compon
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {}
+export class LayoutComponent implements AfterViewInit {
+  protected readonly cw = viewChild.required<ElementRef<HTMLDivElement>>('cw');
+  private readonly renderer = inject(Renderer2);
+
+  public ngAfterViewInit(): void {
+    this.renderer.setStyle(this.cw().nativeElement, 'transition', 'var(--layout-transition)');
+  }
+}
